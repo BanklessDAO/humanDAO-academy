048b32c24ac933569671c5efefbdfb1f47176790
Didier Krux <didier@krux.co>
2022-06-28 15:21:13 +0100

    customize humanDAO whitelabel

--- src/constants/whitelabel.ts
+++ src/constants/whitelabel.ts
@@ -1,3 +1,13 @@
 import { WhitelabelType } from 'entities/whitelabel'
 
-export const WHITELABEL: WhitelabelType = {}
+export const WHITELABEL: WhitelabelType = {
+  project_name: 'humanDAO Academy',
+  domain_prod: 'humandao.banklessacademy.com',
+  default_metadata_description: 'Improve lives through crypto',
+  default_metadata_image: '/humanDAO/humanDAO.png',
+  favicon: '/humanDAO/favicon.png',
+  logo: '/humanDAO/HDAO-white.svg',
+  logo_small: '/humanDAO/HDAO-white.svg',
+  homepage_background: '/humanDAO/homepage_background.jpg',
+  umami_prod: 'a50ccb31-0534-407f-aebe-312bd64b2689',
+}
